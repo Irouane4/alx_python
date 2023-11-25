31aa78079e55f8b0b99dd483f1996bd577cbbae1
Irouane1 <itsirw@gmail.com>
2023-11-25 16:38:05 +0100

    commit

---
+++ python-hello_world/test.py
@@ -0,0 +1,13 @@
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+last_digit *= -1 if number < 0 else 1
+print(f"Last digit of {number} is {last_digit} and")
+if last_digit > 5:
+    print("is greater than 5")
+elif last_digit == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
+
+
