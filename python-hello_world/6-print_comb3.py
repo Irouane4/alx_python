114167793bb2a7c983267a2ea051aa9753be115c
Irouane1 <itsirw@gmail.com>
2023-11-25 17:51:29 +0100

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,4 @@
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=", " if j < 9 else "\n" if i == 8 and j == 9 else " ")
+        print("{:02d}, {:02d}, ".format(i, j), end="")
+print("{:02d}".format(9))
