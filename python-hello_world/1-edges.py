ae7f0e099900cc3dea4aea6c85d2f44a3d4e2134
Irouane1 <itsirw@gmail.com>
2023-11-25 12:29:34 +0100

    commit

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,6 @@
+word = "Holberton"
+word_first_3, *_, word_last_2 = word
+middle_word = "".join(_)
+print("First 3 letters: {}".format("".join(word_first_3)))
+print("Last 2 letters: {}".format("".join(word_last_2)))
+print("Middle word: {}".format(middle_word))
