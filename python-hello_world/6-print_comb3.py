6d003ee97ef1806ce3ff089936101872e5ce91b0
Irouane1 <itsirw@gmail.com>
2023-11-25 18:02:25 +0100

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,10 +1,17 @@
 count = 0
-for num in range(1, 100):
+
+for num in range(10, 100):
+
     first_digit = num // 10
     second_digit = num % 10
+
     if first_digit != second_digit:
+
         if count > 0:
             print(", ", end="")
+
         print("{:02d}".format(num), end="")
+
         count += 1
+
 print()
