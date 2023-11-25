b70edbb1ba5fc2921d6cec995b2a9a89697b6d3b
Irouane1 <itsirw@gmail.com>
2023-11-25 12:55:13 +0100

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,8 @@
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-word_first_3, *middle_word, word_last_2 = word
-print("First 3 letters: {}".format("".join(word_first_3)))
-print("Last 2 letters: {}".format("".join(word_last_2)))
-print("Middle word: {}".format("".join(middle_word)))
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[3:-2]
+
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
