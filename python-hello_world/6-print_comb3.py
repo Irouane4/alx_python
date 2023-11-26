aeb88deedaa0970bcd45bb4658b3cd1a43d04f22
Irouane1 <itsirw@gmail.com>
2023-11-26 13:11:29 +0100

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,4 @@
-def print_combinations():
-    for i in range(10):
-        for j in range(i+1, 10):
-            print("{:02d}, ".format(i), end="")
-            print("{:02d}, ".format(j), end="")
-    print("89")
-
-print_combinations()
+for i in range(10):
+    for j in range(i + 1, 10):
+        is_last_number = i == 8 and j == 9  # Check if it's the last number (89)
+        print("{:02d}".format(i * 10 + j), end="" if is_last_number else ", ")
