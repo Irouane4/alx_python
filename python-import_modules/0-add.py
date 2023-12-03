025347cb1913fa1ae8be72b92bbd64538272fd29
Irouane1 <itsirw@gmail.com>
2023-12-03 18:58:34 +0100

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,6 @@ import importlib
 a = 1
 b = 2
 
-add = importlib.import_module('add_0').add
-print("{} + {} = {}".format(a, b, add(a, b)))
-
+add_0 = importlib.import_module('add_0')
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
