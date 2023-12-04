95b04f2fcd107483437424b346442635705183f1
Irouane1 <itsirw@gmail.com>
2023-12-04 13:25:07 +0100

    commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,3 @@
-# 1-print_matrix_integer.py
-
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return
@@ -11,14 +9,12 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{:d} ".format(element), end="")
         print()
-
 if __name__ == "__main__":
     matrix = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
     ]
-
     print_matrix_integer(matrix)
     print("--")
     print_matrix_integer()
