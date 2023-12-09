7aeaa17d09294fa28406fa9772e241091b51fbd6
Irouane1 <itsirw@gmail.com>
2023-12-09 19:19:40 +0100

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -12,25 +12,3 @@ class Square:
             size (int): The size of the square.
         """
         self.__size = size
-
-# Test the Square class
-if __name__ == "__main__":
-    my_square = Square(3)
-
-    # Print the type of my_square
-    print(type(my_square))
-
-    # Print the dictionary representation of my_square
-    print(my_square.__dict__)
-
-    # Attempt to access the size attribute (should raise an error)
-    try:
-        print(my_square.size)
-    except AttributeError as e:
-        print(e)
-
-    # Attempt to access the __size attribute (should raise an error)
-    try:
-        print(my_square.__size)
-    except AttributeError as e:
-        print(e)
