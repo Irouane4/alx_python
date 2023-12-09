4c365506327e3a3174ec8241f5cd347c952f0dba
Irouane1 <itsirw@gmail.com>
2023-12-09 14:39:36 +0100

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,30 @@
 class Square:
     """A class that defines a square.
+
     Attributes:
-        __size (int): The size of the square."""
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
         """Initialize a square with a given size.
+
         Args:
-            size (int): The size of the square."""
+            size (int): The size of the square.
+        """
         self.__size = size
+
 if __name__ == "__main__":
     my_square = Square(3)
+
     print(type(my_square))
+
     print(my_square.__dict__)
+
     try:
         print(my_square.size)
     except AttributeError as e:
         print(e)
+
     try:
         print(my_square.__size)
     except AttributeError as e:
