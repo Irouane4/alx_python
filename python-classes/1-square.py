b01f26638ea17e1a0914521d68dfd04176f64c72
Irouane1 <itsirw@gmail.com>
2023-12-09 20:25:16 +0100

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,63 @@
 class Square:
-    """Defines a square."""
+    """
+    A class that defines a square.
+
+    Attributes:
+    __size (int): The size of the square.
+
+    Methods:
+    __init__(self, size=0): Initializes a new instance of the Square class.
+    """
+
     def __init__(self, size=0):
-        """Initializes a new Square instance.
+        """
+        Initializes a new instance of the Square class.
+
         Args:
-            size (int, optional): The size of the square. Defaults to 0.
+        size (int): The size of the square. Default is 0.
+
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
         """
         self.__size = size
-        # Validate the size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
+
+if __name__ == "__main__":
+    # Test cases
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
