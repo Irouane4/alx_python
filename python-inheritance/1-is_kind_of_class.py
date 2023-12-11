8c672d80be9606a9f3a31d54f68651ba7c15b57a
Irouane1 <itsirw@gmail.com>
2023-12-11 17:38:17 +0100

    commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -3,12 +3,12 @@ def is_kind_of_class(obj, a_class):
     Returns True if the object is an instance of, or if the object is an instance of a class that inherited from,
     the specified class; otherwise False.
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class or issubclass(type(obj), a_class)
 
 a = 1
 if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
+    print("True")
 if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
+    print("True")
 if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+    print("True")
