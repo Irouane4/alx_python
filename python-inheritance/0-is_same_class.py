4cb207fa08ba8ded5db73ab1be051289ff8a52b4
Irouane1 <itsirw@gmail.com>
2023-12-11 15:55:18 +0100

    commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -6,29 +6,29 @@ def is_same_class(obj, a_class):
     :param a_class: The class to check against.
     :return: True if obj is an instance of a_class; otherwise False.
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
 
 # Test cases
 a = 1
-print(is_same_class(a, int))
+print(is_same_class(a, int))  # Expected output: True
 
 a = True
-print(is_same_class(a, int))
+print(is_same_class(a, int))  # Expected output: False
 
 a = 3.14
-print(is_same_class(a, int))
+print(is_same_class(a, int))  # Expected output: False
 
 a = True
-print(is_same_class(a, object))
+print(is_same_class(a, object))  # Expected output: False
 
 a = None
-print(is_same_class(a, object))
+print(is_same_class(a, object))  # Expected output: False
 
 a = None
-print(is_same_class(a, list))
+print(is_same_class(a, list))  # Expected output: False
 
 a = [1, 2, 3]
-print(is_same_class(a, list))
+print(is_same_class(a, list))  # Expected output: True
 
 a = [1, 2, 3]
-print(is_same_class(a, object))
+print(is_same_class(a, object))  # Expected output: False
