6ad81f0cb04cce43120091122442283ed57559b0
Irouane1 <itsirw@gmail.com>
2023-12-11 15:37:06 +0100

    commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,38 +1,18 @@
 def is_same_class(obj, a_class):
     """
-    Check if the object is exactly an instance of the specified class.
+    Returns True if the object is exactly an instance of the specified class; otherwise False.
 
-    Args:
-        obj: Object to be checked.
-        a_class: Class to compare against.
-
-    Returns:
-        True if obj is an instance of a_class; otherwise, False.
+    :param obj: The object to check.
+    :param a_class: The class to check against.
+    :return: True if obj is an instance of a_class; otherwise False.
     """
     return type(obj) is a_class
 
-if __name__ == "__main__":
-    # Test cases
-    a = 1
-    print(is_same_class(a, int))  # True
-
-    a = True
-    print(is_same_class(a, int))  # False
-
-    a = 3.14
-    print(is_same_class(a, int))  # False
-
-    a = True
-    print(is_same_class(a, object))  # False
-
-    a = None
-    print(is_same_class(a, object))  # False
-
-    a = None
-    print(is_same_class(a, list))  # False
-
-    a = [1, 2, 3]
-    print(is_same_class(a, list))  # False
-
-    a = [1, 2, 3]
-    print(is_same_class(a, object))  # False
+# Test cases
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
