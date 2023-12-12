44c89b1c37da034381240e7a0d5fb0a1f5432ff2
Irouane1 <itsirw@gmail.com>
2023-12-12 18:41:22 +0100

    commit

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -3,8 +3,16 @@ BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# Expected output including 'area'
+expected_output = [
+    '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
+    '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+    '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+    '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area'
+]
 
+# Check if all elements in the expected output are in the actual output
+if all(attr in dir(bg) for attr in expected_output):
+    print("Output matches the expected output!")
+else:
+    print("Output does not match the expected output.")
