33a4fa4e24c53a89249879b8980542ad16c64608
Irouane1 <itsirw@gmail.com>
2023-12-12 18:23:47 +0100

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,3 +1,9 @@
+"""
+Module: 3-base_geometry
+
+This module contains the definition of an empty class, BaseGeometry.
+"""
+
 class BaseGeometry:
     """
     Empty class definition for BaseGeometry.
