4eb667d73c56cd3752ef6f3a54ac23bc6bb315b7
Irouane1 <itsirw@gmail.com>
2023-12-12 17:58:16 +0100

    commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -10,33 +10,3 @@ def is_kind_of_class(obj, a_class):
     :return: True if obj is an instance of a_class or its subclasses; otherwise, False.
     """
     return isinstance(obj, a_class)
-
-if __name__ == "__main__":
-    # Example usage
-    a = 1
-    if is_kind_of_class(a, int):
-        print("{} comes from {}".format(a, int.__name__))
-
-    a = True
-    if is_kind_of_class(a, int):
-        print("{} comes from {}".format(a, int.__name__))
-    if is_kind_of_class(a, object):
-        print("{} comes from {}".format(a, object.__name__))
-
-    a = 3.14
-    if is_kind_of_class(a, int):
-        print("{} comes from {}".format(a, int.__name__))
-    if is_kind_of_class(a, object):
-        print("{} comes from {}".format(a, object.__name__))
-
-    a = None
-    if is_kind_of_class(a, object):
-        print("{} comes from {}".format(a, object.__name__))
-    if is_kind_of_class(a, list):
-        print("{} comes from {}".format(a, list.__name__))
-
-    a = [1, 2, 3]
-    if is_kind_of_class(a, list):
-        print("{} comes from {}".format(a, list.__name__))
-    if is_kind_of_class(a, object):
-        print("{} comes from {}".format(a, object.__name__))
--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 is_kind_of_class = __import__('1-is_kind_of_class').is_kind_of_class
 
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
+if __name__ == "__main__":
+    a = 1
+    print(is_kind_of_class(a, int))
 
-a = True
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+    a = True
+    print(is_kind_of_class(a, int))
+    print(is_kind_of_class(a, object))
 
-a = 3.14
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+    a = 3.14
+    print(is_kind_of_class(a, int))
+    print(is_kind_of_class(a, object))
 
-a = None
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
-if is_kind_of_class(a, list):
-    print("{} comes from {}".format(a, list.__name__))
+    a = None
+    print(is_kind_of_class(a, object))
+    print(is_kind_of_class(a, list))
 
-a = [1, 2, 3]
-if is_kind_of_class(a, list):
-    print("{} comes from {}".format(a, list.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+    a = [1, 2, 3]
+    print(is_kind_of_class(a, list))
+    print(is_kind_of_class(a, object))
