74af03a08d71224dc6e19e774545fdcc6d5fe973
Irouane1 <itsirw@gmail.com>
2023-12-12 18:13:11 +0100

    commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,12 +1,20 @@
-# 1-is_kind_of_class.py
+"""
+Module: 1-is_kind_of_class
+
+This module contains a function that checks if an object is an instance of, or if
+the object is an instance of a class that inherited from, the specified class.
+"""
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if the object is an instance of, or if the object is an instance
-    of a class that inherited from, the specified class.
+    Check if the object is an instance of, or if the object is an instance of
+    a class that inherited from, the specified class.
 
-    :param obj: The object to check.
-    :param a_class: The class to check against.
-    :return: True if obj is an instance of a_class or its subclasses; otherwise, False.
+    Args:
+    - obj: The object to check.
+    - a_class: The class to compare against.
+
+    Returns:
+    - True if obj is an instance of a_class or inherited from it; False otherwise.
     """
     return isinstance(obj, a_class)
--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 is_kind_of_class = __import__('1-is_kind_of_class').is_kind_of_class
 
-if __name__ == "__main__":
-    a = 1
-    print(is_kind_of_class(a, int))
-
-    a = True
-    print(is_kind_of_class(a, int))
-    print(is_kind_of_class(a, object))
-
-    a = 3.14
-    print(is_kind_of_class(a, int))
-    print(is_kind_of_class(a, object))
-
-    a = None
-    print(is_kind_of_class(a, object))
-    print(is_kind_of_class(a, list))
-
-    a = [1, 2, 3]
-    print(is_kind_of_class(a, list))
-    print(is_kind_of_class(a, object))
+a = 1
+if is_kind_of_class(a, int):
+    print("{} comes from {}".format(a, int.__name__))
+if is_kind_of_class(a, float):
+    print("{} comes from {}".format(a, float.__name__))
+if is_kind_of_class(a, object):
+    print("{} comes from {}".format(a, object.__name__))
