103455320e9919990e4e2804c5071b4ccb9f7215
Irouane1 <itsirw@gmail.com>
2023-12-12 18:19:28 +0100

    commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -15,6 +15,6 @@ def inherits_from(obj, a_class):
     - a_class: The class to compare against.
 
     Returns:
-    - True if obj is an instance of a class that inherited from a_class; False otherwise.
+    - True if obj is an instance of a subclass of a_class; False otherwise.
     """
-    return issubclass(type(obj), a_class)
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
