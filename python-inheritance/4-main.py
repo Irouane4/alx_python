96cc78a5b1348ff0076072ccbbdb90ae25fcc476
Irouane1 <itsirw@gmail.com>
2023-12-13 19:23:39 +0100

    commit

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -7,7 +7,13 @@ bg = BaseGeometry()
 actual_output = dir(bg)
 
 # Exclude '__init_subclass__' from the expected output
-expected_output = [attr for attr in actual_output if attr != '__init_subclass__']
+expected_output = [
+    '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
+    '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+    '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+    '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+    'area'
+]
 
 # Check if the output matches the expected output
 if actual_output == expected_output:
