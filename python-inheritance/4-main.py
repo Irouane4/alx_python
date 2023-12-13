90b8a604d09b3c94d6feeca24e691bfbf460a853
Irouane1 <itsirw@gmail.com>
2023-12-13 19:21:50 +0100

    commit

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -1,17 +1,16 @@
+#!/usr/bin/python3
 BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-expected_output = [
-    '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
-    '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-    '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
-    '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
-    'area'
-]
+# Get the actual output from dir(bg)
+actual_output = dir(bg)
 
-if all(attr in dir(bg) for attr in expected_output):
+# Exclude '__init_subclass__' from the expected output
+expected_output = [attr for attr in actual_output if attr != '__init_subclass__']
+
+# Check if the output matches the expected output
+if actual_output == expected_output:
     print("Output matches the expected output!")
 else:
     print("Output does not match the expected output.")
-
