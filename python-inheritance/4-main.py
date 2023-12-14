c51ff8ff82c1c814c8dbcbe279944d8c1f68385d
Irouane1 <itsirw@gmail.com>
2023-12-14 13:30:26 +0100

    commit

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -3,23 +3,15 @@ BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-# Get the actual output from dir(bg)
-actual_output = dir(bg)
+# Expected attributes that should be present in the output
+expected_attributes = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__',
+                        '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__',
+                        '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__',
+                        '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
+                        '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area']
 
-# Remove '__init_subclass__' from the actual output
-actual_output = [attr for attr in actual_output if attr != '__init_subclass__']
-
-# Exclude '__init_subclass__' from the expected output
-expected_output = [
-    '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
-    '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-    '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
-    '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
-    'area'
-]
-
-# Check if the output matches the expected output
-if actual_output == expected_output:
+# Check if all expected attributes are present in the actual output
+if all(attr in dir(bg) for attr in expected_attributes):
     print("Output matches the expected output!")
 else:
     print("Output does not match the expected output.")
