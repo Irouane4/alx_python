29bd87851706b6a2e46e296ddc6743e7da85f90f
Irouane1 <itsirw@gmail.com>
2023-12-19 15:45:06 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,16 +21,9 @@ class Rectangle(Base):
         Note:
             This method updates the attributes with the values provided in the arguments.
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        attributes = ["id", "width", "height", "x", "y"]
+        for i, value in enumerate(args):
+            setattr(self, attributes[i], value)
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
