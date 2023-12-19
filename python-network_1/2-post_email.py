129fbe40abf845aac1d1f4619c91707427330eae
Irouane1 <itsirw@gmail.com>
2023-12-19 18:19:27 +0100

    commit

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,30 @@
+"""This script sends a POST request with an email parameter to a specified URL"""
+
+import requests
+import sys
+
+
+def post_email(url, email):
+    """
+    Sends a POST request with an email parameter to the specified URL
+    and displays the body of the response.
+
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to include in the request parameters.
+    """
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print(f"Your email is: {email}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_email(url, email)
