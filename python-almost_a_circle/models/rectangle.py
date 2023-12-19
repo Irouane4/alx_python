44e5c4037b1f5224ed3820d985375816860e11de
Irouane1 <itsirw@gmail.com>
2023-12-19 17:45:33 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """ Rectangle module """
 from models.base import Base
 
+
 class Rectangle(Base):
     """ Rectangle class """
 
@@ -13,15 +14,67 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    @property
+    def width(self):
+        """ Getter method for width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter method for width attribute """
+        self.validate_integer(value, "width")
+        self.validate_positive(value, "width")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter method for height attribute """
+        self.validate_integer(value, "height")
+        self.validate_positive(value, "height")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter method for x attribute """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter method for x attribute """
+        self.validate_integer(value, "x")
+        self.validate_non_negative(value, "x")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter method for y attribute """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter method for y attribute """
+        self.validate_integer(value, "y")
+        self.validate_non_negative(value, "y")
+        self.__y = value
+
+    def area(self):
+        """ Public method that returns the area value of the Rectangle instance """
+        return self.width * self.height
+
     def display(self):
-        """ Prints the Rectangle with '#' characters """
+        """ Public method that prints in stdout the Rectangle instance with the character '#' """
         for _ in range(self.y):
             print()
         for _ in range(self.height):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        """ Returns a string representation of the Rectangle instance """
+        """ Override of the __str__ method to return a formatted string """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
@@ -36,42 +89,17 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    @property
-    def width(self):
-        """ Getter for width """
-        return self.__width
+    def validate_positive(self, value, attr_name):
+        """ Private method to validate if a value is positive """
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(attr_name))
 
-    @width.setter
-    def width(self, value):
-        """ Setter for width """
-        self.__width = value
+    def validate_non_negative(self, value, attr_name):
+        """ Private method to validate if a value is non-negative """
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(attr_name))
 
-    @property
-    def height(self):
-        """ Getter for height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Setter for height """
-        self.__height = value
-
-    @property
-    def x(self):
-        """ Getter for x """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ Setter for x """
-        self.__x = value
-
-    @property
-    def y(self):
-        """ Getter for y """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ Setter for y """
-        self.__y = value
+    def validate_integer(self, value, attr_name):
+        """ Private method to validate if a value is an integer """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(attr_name))
