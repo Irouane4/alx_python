da77297c379e1ca33a49060d852fc0cb1ef2b63b
Irouane1 <itsirw@gmail.com>
2023-12-21 17:20:33 +0100

    commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,30 @@
-#!/usr/bin/python3
-"""Script to send a POST request to a URL with an email parameter"""
-
 import requests
 import sys
 
+
+def post_email(url, email):
+    # Define the payload with the email parameter
+    payload = {'email': email}
+
+    # Send a POST request to the specified URL with the payload
+    response = requests.post(url, data=payload)
+
+    # Display the body of the response
+    print(response.text)
+
+
 if __name__ == "__main__":
+    # Check if the correct number of command-line arguments are provided
+    if len(sys.argv) != 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract the URL and email from command-line arguments
     url = sys.argv[1]
     email = sys.argv[2]
 
-    response = requests.post(url, data={'email': email})
-    print("Your email is: {}".format(response.text))
+    # Display the email
+    print("Your email is:", email)
+
+    # Call the function to send the POST request and display the response
+    post_email(url, email)
