2e6aab21f409e313d32153798dcb8c5851637685
Irouane1 <itsirw@gmail.com>
2024-01-18 18:18:52 +0100

    c

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+import sys
+from sqlalchemy import (create_engine, Column, Integer, String, ForeignKey,
+                        CheckConstraint, UniqueConstraint)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return "<State(id='%s', name='%s')>" % (self.id, self.name)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
