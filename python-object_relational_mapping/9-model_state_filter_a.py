f04933570778385fa2520a2f905113cc30523933
Irouane1 <itsirw@gmail.com>
2024-01-18 19:00:52 +0100

    c

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,22 +1,28 @@
+#!/usr/bin/env python3
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def main():
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
+def filter_states(user, password, database):
+    # Create a MySQL engine
+    engine = create_engine(f'mysql+pymysql://{user}:{password}@localhost:3306/{database}')
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id.asc()).all()
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    if states:
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
+    # Query all State objects with the letter a
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print("Usage: {} user password database".format(sys.argv[0]))
+        exit(1)
+    user, password, database = sys.argv[1:4]
+    filter_states(user, password, database)
     
