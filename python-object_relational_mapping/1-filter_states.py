73e70192d31931ac881120066640bf96dbc83594
Irouane1 <itsirw@gmail.com>
2024-01-18 17:09:01 +0100

    c

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+"""
 
 import sys
 import MySQLdb
 
-def filter_states(username, password, database):
-    """Connects to a MySQL server and lists all states in the hbtn_0e_0_usa database with names starting with N."""
+if __name__ == "__main__":
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    db = MySQLdb.connect(user=username, passwd=password, db=database, host="localhost", port=3306)
+    # Get command line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a MySQL cursor
     cursor = db.cursor()
 
-    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute the SQL query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    # Fetch all the rows
     rows = cursor.fetchall()
 
+    # Display the results
     for row in rows:
         print(row)
 
+    # Close cursor and database connection
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
