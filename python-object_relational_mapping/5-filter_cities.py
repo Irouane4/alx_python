20bc06eac7ecc93bf848bd2028994df52dc242fa
Irouane1 <itsirw@gmail.com>
2024-01-18 18:41:40 +0100

    c

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,23 @@
-import pymysql
+import MySQLdb
 
 # Connection
-conn = pymysql.connect(
-    host='localhost',
+db = MySQLdb.connect(
+    host="localhost",
     port=3306,
-    user='user',
-    password='password',
-    db='test_5',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
+    user="user",
+    password="password",
+    db="hbtn_0e_4_usa",
+    charset="utf8mb4"
 )
 
 # Function to fetch city names
 def fetch_city_names(state_name):
-    with conn.cursor() as cursor:
-        query = "SELECT cities.id, cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+    with db.cursor() as cursor:
+        query = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
         cursor.execute(query, (state_name,))
         result = cursor.fetchall()
-        return result
+        for row in result:
+            print(row[0])
 
 # Call the function
-cities = fetch_city_names("Arizona")
-for city in cities:
-    print(city['name'])
-    
+fetch_city_names("Arizona")
