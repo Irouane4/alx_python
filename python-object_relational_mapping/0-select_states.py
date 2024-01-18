9a2eab2a207a533b9f28b93b2bbf453f8461c1bd
Irouane1 <itsirw@gmail.com>
2024-01-18 16:57:56 +0100

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,23 @@
-"""
-Script that lists all states from the database hbtn_0e_0_usa.
-"""
+#!/usr/bin/env python3
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+def list_states(username, password, database):
+    """Connects to a MySQL server and lists all states in the hbtn_0e_0_usa database."""
 
-    # Get command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a MySQL cursor
+    db = MySQLdb.connect(user=username, passwd=password, db=database, host="localhost", port=3306)
     cursor = db.cursor()
 
-    # Execute the SQL query
-    cursor.execute("SELECT * FROM states ORDER BY id")
-
-    # Fetch all the rows
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
 
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    
