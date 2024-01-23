432f9d094dc3111840cd41ec95343eab97f36c0d
Irouane1 <itsirw@gmail.com>
2024-01-23 18:42:33 +0100

    c

--- python-web_framework/8-add_retrieve_users.py
+++ python-web_framework/8-add_retrieve_users.py
@@ -27,8 +27,8 @@ def index():
 def add_user():
     if request.method == 'POST':
         try:
-            name = request.form['name']
-            email = request.form['email']
+            name = request.form.get('name')
+            email = request.form.get('email')
             new_user = User(name=name, email=email)
             db.session.add(new_user)
             db.session.commit()
